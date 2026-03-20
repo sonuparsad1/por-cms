7965952838a89bb4087977ecedb0f3a857e8b116
Sonu parsad <sonusa470@gmail.com>
2026-03-20 11:40:22 +0530

    fix: correct relative import path in VisualThemeInjector.jsx for Vercel build

--- client/src/components/layout/VisualThemeInjector.jsx
+++ client/src/components/layout/VisualThemeInjector.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react';
-import { SettingsContext } from '../contexts/SettingsContext';
+import { SettingsContext } from '../../contexts/SettingsContext';
 
 const VisualThemeInjector = () => {
     const { settings } = useContext(SettingsContext);
